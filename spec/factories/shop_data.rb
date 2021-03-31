3a7704802378fe35b8438f7d2661029a89856189
taka-elle <takaelle0119@gmail.com>
2021-03-31 14:07:15 +0900

    rspecテスト

--- spec/factories/shop_data.rb
+++ spec/factories/shop_data.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :shop_datum do
-    
+    text         {Faker::Lorem.paragraph}
+    association :shop
+
+    after(:build) do |shop_datum|
+      shop_datum.image1.attach(io: File.open('app/assets/images/cocomic-shop-demo.jpg'), filename: 'cocomic-shop-demo.jpg', content_type: 'image/jpg')
+      shop_datum.image2.attach(io: File.open('app/assets/images/cocomic-shop-demo.jpg'), filename: 'cocomic-shop-demo.jpg', content_type: 'image/jpg')
+      shop_datum.image3.attach(io: File.open('app/assets/images/cocomic-shop-demo.jpg'), filename: 'cocomic-shop-demo.jpg', content_type: 'image/jpg')
+      shop_datum.image4.attach(io: File.open('app/assets/images/cocomic-shop-demo.jpg'), filename: 'cocomic-shop-demo.jpg', content_type: 'image/jpg')
+      shop_datum.image5.attach(io: File.open('app/assets/images/cocomic-shop-demo.jpg'), filename: 'cocomic-shop-demo.jpg', content_type: 'image/jpg')
+    end
   end
 end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -4,75 +4,64 @@ RSpec.describe 'ユーザー新規登録', type: :system do
   before do
     @user = FactoryBot.build(:user)
     @shop = FactoryBot.create(:shop)
+    @shop_data = FactoryBot.create(:shop_datum, shop: @shop)
     sleep(0.1)
   end
   context 'ユーザー新規登録ができるとき' do 
     it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
-      # トップページに移動する
+      # トップページから新規ページへ移動
       visit root_path
-      #「閉じる」ボタンでモーダルウィンドウを閉じる
       expect(
           find(".load-modal-close").click)
-      # トップページにサインアップページへ遷移するボタンがあることを確認する
       expect(page).to have_content("新規登録")
-      # 新規登録ページへ移動する
       visit new_user_registration_path
-      # ユーザー情報を入力する
+
+      # ユーザー情報を入力し、モデルのカウントが増えることを確認
       fill_in 'ニックネーム', with: @user.name
       fill_in 'メールアドレス', with: @user.email
       fill_in 'パスワード', with: @user.password
       fill_in 'パスワード(確認用)', with: @user.password_confirmation
-      # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
       expect{
         find('input[name="commit"]').click
       }.to change {User.count}.by(1)
-      # itemsトップページへ遷移する
         expect(current_path).to eq items_path
-      # ユーザー名をクリックするとメニューが開く
+
+      # ログアウトボタンを確認
       expect(
           find(".login-name").click
         )
-      # メニューにログアウトへ遷移するボタンがあることを確認する
       expect(page).to have_content('ログアウト')
     end
   end
   context 'ユーザー新規登録ができないとき' do
     it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
-      # トップページに移動する
+      # トップページから新規登録ページへ移動
       visit root_path
-      #「閉じる」ボタンでモーダルウィンドウを閉じる
       expect(
           find(".load-modal-close").click)
-      # トップページにサインアップページへ遷移するボタンがあることを確認する
       expect(page).to have_content('新規登録')
-      # 新規登録ページへ移動する
       visit new_user_registration_path
-      # ユーザー情報を入力する
+
+      # 入力欄を空で登録してもモデルのカウントが上がらないことを確認
       fill_in 'ニックネーム',with:""
       fill_in 'メールアドレス',with:""
       fill_in 'パスワード',with:""
       fill_in 'パスワード(確認用)',with:""
-      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
       expect{
         find('input[name="commit"]').click
       }.to change {User.count}.by(0)
-      # 新規登録ページへ戻されることを確認する
       expect(current_path).to eq "/users"
     end
     it 'Shopで既にログインしているとき、メニューとトップ画像内の新規登録ボタンがなくなっている'do
-      #Shopでログイン
-      sign_in_shop(@shop)
-
-      #トップページへ移動し、「新規登録」ボタンがなくなっているのを確認する
+    #トップページへ移動し、「新規登録」ボタンがなくなっているのを確認する
+    sign_in_shop(@shop)
       visit root_path
       expect(find(".load-modal-close").click)
       expect(page).to have_no_content('新規登録')
     end
     it 'Shopで既にログインしているとき、トップ画面の「はじめる」ボタンをおしてもトップ画面へ遷移される'do
-      #Shopでログイン
-      sign_in_shop(@shop)
-
       #トップページへ移動し、「はじめる」ボタンがあるのを確認する
+      sign_in_shop(@shop)
       visit root_path
       expect(find(".load-modal-close").click)
       expect(page).to have_content('はじめる')
@@ -88,62 +77,51 @@ RSpec.describe 'ログイン', type: :system do
   before do
     @user = FactoryBot.create(:user)
     @shop = FactoryBot.create(:shop)
+    @shop_data = FactoryBot.create(:shop_datum, shop: @shop)
   end
   context 'ログインができるとき' do
     it '保存されているユーザーの情報と合致すればログインができる' do
-      # トップページに移動する
+      # トップページからログインページへ移動する
       visit root_path
-      #「閉じる」ボタンでモーダルウィンドウを閉じる
       expect(
           find(".load-modal-close").click)
-      # トップページにログインページへ遷移するボタンがあることを確認する
       expect(page).to have_content('ログイン')
-      # ログインページへ遷移する
       visit new_user_session_path
-      # 正しいユーザー情報を入力する
+
+      # 正しいユーザー情報を入力するしログイン
       fill_in 'メールアドレス',with:@user.email
       fill_in 'パスワード',with:@user.password
-      # ログインボタンを押す
       find('input[name="commit"]').click
-      # itemsトップページへ遷移することを確認する
       expect(current_path).to eq items_path
-      # ユーザー名をクリックするとメニューが開く
+
+      # ログアウトボタンを確認
       expect(
           find(".login-name").click
         )
-      # メニューにログアウトへ遷移するボタンがあることを確認する
       expect(page).to have_content('ログアウト')
     end
   end
   context 'ログインができないとき' do
     it '保存されているユーザーの情報と合致しないとログインができない' do
-      # トップページに移動する
+      # トップページからログインページへ移動
       visit root_path
-      #「閉じる」ボタンでモーダルウィンドウを閉じる
       expect(
           find(".load-modal-close").click)
-      # トップページにログインページへ遷移するボタンがあることを確認する
       expect(page).to have_content('ログイン')
-      # ログインページへ遷移する
       visit new_user_session_path
-      # ユーザー情報を入力する
+
+      # 謝ったユーザー情報を入力してもログインされない
       fill_in 'メールアドレス',with:""
       fill_in 'パスワード',with:""
-      # ログインボタンを押す
       find('input[name="commit"]').click
-      # ログインページへ戻されることを確認する
       expect(current_path).to eq new_user_session_path
     end
     it 'Shopで既にログインしているとき、トップページのログインボタンがログアウトボタンに切り替わっている'do
-      #Shopでログイン
-      sign_in_shop(@shop)
-
-      #トップページへ移動し、「ログイン」ボタンがなくなっているのを確認する
+    #トップページへ移動し、「ログイン」ボタンがなくなっているのを確認する
+    sign_in_shop(@shop)
       visit root_path
       expect(find(".load-modal-close").click)
       expect(page).to have_no_content('ログイン')
-
-      #「ログアウト」ボタンが新しく表示されていることを確認する
       expect(page).to have_content('ログアウト')
     end
   end
